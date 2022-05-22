7cb4fb84a6d68649ea5699f559d6265563f7bd41
xyw <2453986438@qq.com>
2022-05-22 14:43:39 +0800

    Initial commit

---
+++ src/main/java/com/at/controller/MyController.java
@@ -0,0 +1,14 @@
+package com.at.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World！";
+    }
+}
